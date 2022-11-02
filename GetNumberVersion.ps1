79572af5fa03f4a70f365e14dc2a4cc1e0c24f07
Erik Mogensen <ErikMogensen@users.noreply.github.com>
2022-11-02 10:57:57 +0100

    Update script

--- GetNumberVersion.ps1
+++ GetNumberVersion.ps1
@@ -2,19 +2,19 @@
 param(
     [Parameter(Mandatory=$false)]
 	[string]
-    $ReleaseVersion
+    $Version
 )
 
 Set-StrictMode -Version 3
 
-if ($ReleaseVersion -match "^\d+\.\d+\.\d+$") {
-	return $ReleaseVersion
+if ($Version -match "^\d+\.\d+\.\d+$") {
+	return $Version
 }
 
-if (-not ($ReleaseVersion -match "^\d+\.\d+\.\d+-.{1,12}$")) {
+if (-not ($Version -match "^\d+\.\d+\.\d+-.{1,12}$")) {
 	throw "The version must start with three numbers separated by dots. This may be " + `
 		"followed by a '-' and a word. That word may not be longer than 12 characters. For instance, 4.3.22-beta."
 }
 
-$parts = $ReleaseVersion -split "-"
+$parts = $Version -split "-"
 return $parts[0]
